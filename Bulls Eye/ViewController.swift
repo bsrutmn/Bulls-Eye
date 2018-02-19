90de50214d25a4e3d70191a3c6e330aaf1cd9e40
Mobil Trakya <mobiltrakya@mobils-imac-3.local>
2018-02-19 10:15:37 +0300

    Initial Commit

---
+++ Bulls Eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bulls Eye
+//
+//  Created by Mobil Trakya on 2/19/18.
+//  Copyright © 2018 Trakya University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
